a38fd77ef6f85fbafe054c6dd9bb7d4f523aac8b
chenkai <chenkaihome@live.cn>
2014-11-07 12:22:57 +0800

    first commit empty project

---
+++ Dribbble/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Dribbble
+//
+//  Created by chenkai on 14/11/7.
+//  Copyright (c) 2014年 chenkai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Dribbble/DribbbleAppDelegate.h
@@ -0,0 +1,13 @@
+//
+//  DribbbleAppDelegate.h
+//  Dribbble
+//
+//  Created by chenkai on 14/11/7.
+//  Copyright (c) 2014年 chenkai. All rights reserved.
+//
+
+#ifndef Dribbble_DribbbleAppDelegate_h
+#define Dribbble_DribbbleAppDelegate_h
+
+
+#endif
